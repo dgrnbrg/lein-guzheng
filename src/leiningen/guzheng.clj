51d2c9ebd19f33389900f0d82f56a5dcc0aa6cfa
David Greenberg <dsg123456789@gmail.com>
2012-04-06 21:42:38 -0400

    Why doesn't think work?

--- src/leiningen/guzheng.clj
+++ src/leiningen/guzheng.clj
@@ -1,7 +1,29 @@
-(ns leiningen.guzheng
-  (:use [guzheng.core]))
+(ns leiningen.guzheng)
+
+(defn eval-in-project
+  "Support eval-in-project in both Leiningen 1.x and 2.x."
+  [project form init]
+  (let [[eip two?] (or (try (require 'leiningen.core.eval)
+                         [(resolve 'leiningen.core.eval/eval-in-project)
+                          true]
+                         (catch java.io.FileNotFoundException _))
+                       (try (require 'leiningen.compile)
+                         [(resolve 'leiningen.compile/eval-in-project)]
+                         (catch java.io.FileNotFoundException _)))]
+    (if two?
+      (eip project form init)
+      (eip project form nil nil init))))
 
 (defn guzheng
   "I don't do a lot."
   [project & args]
-  (println @main-trace-atom))
+  (let [project (-> project
+                  (update-in [:dependencies] conj ['guzheng "1.0.0"]))]
+    (eval-in-project project
+                     `(do
+                        (println "lol")
+                        (require 'guzheng.core)
+                        (clojure.pprint/pprint "hi"));@guzheng.core/main-trace-atom)
+                     ;`(require 'guzheng.core)
+                     `(require 'clojure.pprint)
+                     )))
