d7a54a715a34ab746b9b54f83b6edee67de3c9f4
David Greenberg <dsg123456789@gmail.com>
2012-06-10 16:26:28 -0400

    Maybe this works?

--- test-project-lein1/project.clj
+++ test-project-lein1/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[lein-guzheng "0.2.4"]])
+  :dev-dependencies [[lein-guzheng "0.3.1"]])
--- test-project-lein2/project.clj
+++ test-project-lein2/project.clj
@@ -4,5 +4,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :plugins [[lein-guzheng "0.2.4"]]
+  :plugins [[lein-guzheng "0.3.1"]]
   :description "FIXME: write description")
